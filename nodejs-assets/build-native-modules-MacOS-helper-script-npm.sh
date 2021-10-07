75e5d544ec2fd7fa009b629541c0786ade8e4e2d
pierre kraus <pierre@telios.io>
2021-10-07 14:24:33 -0400

    Set up xcode with new native deps

---
+++ nodejs-assets/build-native-modules-MacOS-helper-script-npm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+      # Helper script for Gradle to call npm on macOS in case it is not found
+      export PATH=$PATH:/var/folders/m6/psbrpt_90q1d__rn7m5270480000gn/T/yarn--1633628052964-0.4200791338461414:/Users/telios/Documents/telios-mobile/node_modules/nodejs-mobile-react-native/node_modules/.bin:/Users/telios/.config/yarn/link/node_modules/.bin:/Users/telios/.nvm/versions/node/v12.18.4/libexec/lib/node_modules/npm/bin/node-gyp-bin:/Users/telios/.nvm/versions/node/v12.18.4/lib/node_modules/npm/bin/node-gyp-bin:/Users/telios/.nvm/versions/node/v12.18.4/bin/node_modules/npm/bin/node-gyp-bin:/Users/telios/.nvm/versions/node/v12.18.4/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin
+      npm $@
+    
