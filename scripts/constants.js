de3ec91982cf5763bff205a33c7f046af12096b4
mauve <ranger@mauve.moe>
2021-11-15 20:51:24 -0500

    Change project dir variable :

--- scripts/constants.js
+++ scripts/constants.js
@@ -5,7 +5,7 @@ const ROOT = join(__dirname, '../')
 const ANDROID_DIR = join(ROOT, 'android')
 const IOS_DIR = join(ROOT, 'ios')
 
-const PROJECT_DIR = process.env.PROJECT_DIR || join(ROOT, 'nodejs-assets/nodejs-project/')
+const PROJECT_DIR = process.env.NODEJS_PROJECT_DIR || join(ROOT, 'nodejs-assets/nodejs-project/')
 const MODULE_FOLDER = join(PROJECT_DIR, 'node_modules')
 
 const NODEJS_MOBILE_DIR = join(ROOT, 'node_modules/nodejs-mobile-react-native')
