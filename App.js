e654e036d36eef26fa7da04ddac14115f4012330
mauve <ranger@mauve.moe>
2021-11-23 18:53:55 -0500

    Rework App.js to test nodejs bridge

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import nodejs from 'nodejs-mobile-react-native'
 
-import React, { useEffect } from 'react'
+import React, {
+  useEffect,
+  useState
+} from 'react'
 
 import {
   SafeAreaView,
@@ -8,110 +11,57 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  Button,
   useColorScheme,
   View
 } from 'react-native'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions
-} from 'react-native/Libraries/NewAppScreen'
-
-const Section = ({ children, title }) => {
-  const isDarkMode = useColorScheme() === 'dark'
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black
-          }
-        ]}
-      >
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark
-          }
-        ]}
-      >
-        {children}
-      </Text>
-    </View>
-  )
-}
+const styles = StyleSheet.create({})
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark'
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
-  }
+  const [statusText, setStatusText] = useState('')
+
+  console.log('Rendering', { statusText, isDarkMode })
 
   useEffect(() => {
     nodejs.start('bundle.js')
     nodejs.channel.addListener('message', (msg) => {
       console.log('From node: ', msg)
+      if (msg.event === 'createAccount') {
+        setStatusText(JSON.stringify(msg.data))
+      }
     })
   })
 
+  async function createAccount () {
+    nodejs.channel.send({
+      event: 'createAccount',
+      payload: {
+        password: 'password',
+        email: 'test@telios',
+        recoveryEmail: 'test@telios',
+        vcode: 'betacode'
+      }
+    })
+  }
+
+  const statusElement = statusText ? <Text>{statusText}</Text> : null
+
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
         contentInsetAdjustmentBehavior='automatic'
-        style={backgroundStyle}
       >
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white
-          }}
-        >
-          <Section title='Step One'>
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title='See Your Changes'>
-            <ReloadInstructions />
-          </Section>
-          <Section title='Debug'>
-            <DebugInstructions />
-          </Section>
-          <Section title='Learn More'>
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        <View>
+          <Button title='Create' onPress={createAccount} />
+          {statusElement}
         </View>
       </ScrollView>
     </SafeAreaView>
   )
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600'
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400'
-  },
-  highlight: {
-    fontWeight: '700'
-  }
-})
 
 export default App
